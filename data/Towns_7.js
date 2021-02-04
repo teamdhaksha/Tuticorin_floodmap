77490dc358c62f662e040adbf083c1cbbe9c49eb
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-04 12:17:47 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,13 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Kulasekharapatnam" }, "geometry": { "type": "Point", "coordinates": [ 78.052633661160755, 8.402318187460654, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tiruchendur" }, "geometry": { "type": "Point", "coordinates": [ 78.12300382438498, 8.497013377221567, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tuticorin" }, "geometry": { "type": "Point", "coordinates": [ 78.145745813670359, 8.767402114567213, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kovilpatti" }, "geometry": { "type": "Point", "coordinates": [ 77.866622300372157, 9.172305451532225, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Srivaikuntam" }, "geometry": { "type": "Point", "coordinates": [ 77.915079754170989, 8.632457368987636, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Vilathikulam" }, "geometry": { "type": "Point", "coordinates": [ 78.162005435577001, 9.132418596628531, -999999.0 ] } }
+]
+}
